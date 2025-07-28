f0abc810873843c2d87109a1589793d030a7907f
BodartA <a.bodart23@gmail.com>
2025-07-28 19:56:51 +0200

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Antoine BODART on 28/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
