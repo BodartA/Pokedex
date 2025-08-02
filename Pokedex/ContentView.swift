684c7661141b26ecc7afda270bddf21c2e360f46
BodartA <a.bodart23@gmail.com>
2025-08-02 18:19:28 +0200

    basic feature work.

--- Pokedex/ContentView.swift
+++ Pokedex/ContentView.swift
@@ -11,13 +11,34 @@ struct ContentView: View {
     @StateObject private var viewModel = PokemonViewModel()
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView {
+                    LazyVStack(spacing: 12, pinnedViews: []) {
+                        ForEach(viewModel.pokemons) { pokemon in
+                            HStack(spacing: 16) {
+                                AsyncImage(url: URL(string: pokemon.image)) { image in
+                                    image
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 60, height: 60)
+                                } placeholder: {
+                                    ProgressView()
+                                        .frame(width: 60, height: 60)
+                                }
+                                
+                                Text(pokemon.name)
+                                    .font(.headline)
+                                    .foregroundColor(.primary)
+                                
+                                Spacer()
+                            }
+                            .padding(.horizontal)
+                        }
+                    }
+                    .padding(.top)
+                }
+        .onAppear {
+            viewModel.fetchPokemon()
         }
-        .padding()
     }
 }
 
